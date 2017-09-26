fe5a2d6bb676566770e18fee3a1924ecdfa7dc97
kostiantyn.potapov <kostiantyn.potapov@altium.com>
2017-09-26 09:13:56 +0300

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int a, b, c;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = a + b;
+        System.out.println("ravno " + c);
+    }
+}
